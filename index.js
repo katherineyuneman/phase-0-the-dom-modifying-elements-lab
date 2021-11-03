23d5613365bfcc0d5eaa9c893f0555143dd1af7e
KatherineYuneman <kyuneman@gmail.com>
2021-11-03 18:06:30 -0400

    commit tree c4ccdb68782e1b33705580a6ef3250aad2e10216

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+//remove main
+let main = document.getElementById("main");
+main.remove();
+
+
+
+//add newHeader with h1 element
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+console.log(newHeader.nodeName);
+newHeader.nodeName = "victory";
+console.log(newHeader.nodeName);
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR NAME is the champion."
